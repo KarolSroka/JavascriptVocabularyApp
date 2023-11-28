4d4890a3165cf36dd0198781a496dadc208486f6
KarolSroka <70062296+KarolSroka@users.noreply.github.com>
2023-11-28 22:20:26 +0100

    Add files via upload

---
+++ verbs.js
@@ -0,0 +1,402 @@
+const DATA = [
+    {
+        "front": "ser",
+        "back": "to be (essential/permanent quality)"
+    },
+    {
+        "front": "haber",
+        "back": "to have (to do something, auxiliary verb)"
+    },
+    {
+        "front": "estar",
+        "back": "to be (health, location, state)"
+    },
+    {
+        "front": "tener",
+        "back": "to have"
+    },
+    {
+        "front": "hacer",
+        "back": "to do, to make"
+    },
+    {
+        "front": "poder",
+        "back": "to be able to, can"
+    },
+    {
+        "front": "decir",
+        "back": "to say, to tell"
+    },
+    {
+        "front": "ir",
+        "back": "to go"
+    },
+    {
+        "front": "ver",
+        "back": "to see"
+    },
+    {
+        "front": "dar",
+        "back": "to give"
+    },
+    {
+        "front": "saber",
+        "back": "to know (information)"
+    },
+    {
+        "front": "querer",
+        "back": "to want, to love"
+    },
+    {
+        "front": "llegar",
+        "back": "to arrive, to come, to reach"
+    },
+    {
+        "front": "pasar",
+        "back": "to pass, to spend (time), to happen"
+    },
+    {
+        "front": "deber",
+        "back": "to owe, to must, should, ought to"
+    },
+    {
+        "front": "poner",
+        "back": "to put, to place, to set"
+    },
+    {
+        "front": "parecer",
+        "back": "to seem, to appear"
+    },
+    {
+        "front": "quedar",
+        "back": "to stay, to remain"
+    },
+    {
+        "front": "creer",
+        "back": "to believe"
+    },
+    {
+        "front": "hablar",
+        "back": "to speak"
+    },
+    {
+        "front": "llevar",
+        "back": "to carry, to bring"
+    },
+    {
+        "front": "dejar",
+        "back": "to leave, to abandon, to let, allow"
+    },
+    {
+        "front": "seguir",
+        "back": "to follow, continue"
+    },
+    {
+        "front": "encontrar",
+        "back": "to find, to encounter"
+    },
+    {
+        "front": "llamar",
+        "back": "to call, to name"
+      },
+      {
+        "front": "venir",
+        "back": "to come"
+      },
+      {
+        "front": "pensar",
+        "back": "to think"
+      },
+      {
+        "front": "salir",
+        "back": "to leave, to go out"
+      },
+      {
+        "front": "volver",
+        "back": "to return, to go back"
+      },
+      {
+        "front": "tomar",
+        "back": "to take, to drink"
+      },
+      {
+        "front": "conocer",
+        "back": "to know (people, places)"
+      },
+      {
+        "front": "vivir",
+        "back": "to live"
+      },
+      {
+        "front": "sentir",
+        "back": "to feel, regret"
+      },
+      {
+        "front": "tratar",
+        "back": "to treat, to handle"
+      },
+      {
+        "front": "mirar",
+        "back": "to watch, to look at"
+      },
+      {
+        "front": "contar",
+        "back": "to count, to relate to, to tell"
+      },
+      {
+        "front": "empezar",
+        "back": "to begin, to start"
+      },
+      {
+        "front": "esperar",
+        "back": "to wait for, to hope"
+      },
+      {
+        "front": "buscar",
+        "back": "to search for, to hope"
+      },
+      {
+        "front": "existir",
+        "back": "to exist"
+      },
+      {
+        "front": "entrar",
+        "back": "to enter, to go in, to come in"
+      },
+      {
+        "front": "trabajar",
+        "back": "to work"
+      },
+      {
+        "front": "escribir",
+        "back": "to write"
+      },
+      {
+        "front": "perder",
+        "back": "to lose"
+      },
+      {
+        "front": "producir",
+        "back": "to produce"
+      },
+      {
+        "front": "ocurrir",
+        "back": "to occur, happen"
+      },
+      {
+        "front": "entender",
+        "back": "to understand"
+      },
+      {
+        "front": "pedir",
+        "back": "to request, to ask for"
+      },
+      {
+        "front": "recibir",
+        "back": "to receive, to welcome, to greet"
+      },
+      {
+        "front": "recordar",
+        "back": "to remember, to remind"
+      },
+      {
+        "front": "terminar",
+        "back": "to finish, to end"
+      },
+      {
+        "front": "permitir",
+        "back": "to permit, to allow"
+      },
+      {
+        "front": "aparecer",
+        "back": "to appear, show up"
+      },
+      {
+        "front": "conseguir",
+        "back": "to get, to obtain"
+      },
+      {
+        "front": "comenzar",
+        "back": "to begin, to start, to commence"
+      },
+      {
+        "front": "servir",
+        "back": "to serve"
+      },
+      {
+        "front": "sacar",
+        "back": "to take out, to stick out"
+      },
+      {
+        "front": "necesitar",
+        "back": "to need, to require"
+      },
+      {
+        "front": "mantener",
+        "back": "to maintain, to get"
+      },
+      {
+        "front": "resultar",
+        "back": "to turn out (to be)"
+      },
+      {
+        "front": "leer",
+        "back": "to read"
+      },
+      {
+        "front": "caer",
+        "back": "to fall"
+      },
+      {
+        "front": "cambiar",
+        "back": "to change"
+      },
+      {
+        "front": "presentar",
+        "back": "to introduce"
+      },
+      {
+        "front": "crear",
+        "back": "to create, to make"
+      },
+      {
+        "front": "abrir",
+        "back": "to open"
+      },
+      {
+        "front": "considerar",
+        "back": "to consider"
+      },
+      {
+        "front": "oír",
+        "back": "to hear"
+      },
+      {
+        "front": "acabar",
+        "back": "to finish, to end"
+      },
+      {
+        "front": "convertir",
+        "back": "to convert, to change"
+      },
+      {
+        "front": "ganar",
+        "back": "to win, to gain, to earn, to get, to acquire"
+      },
+      {
+        "front": "formar",
+        "back": "to form, to shape, to fashion, to make"
+      },
+      {
+        "front": "traer",
+        "back": "to bring, to get, to fetch, to carry"
+      },
+      {
+        "front": "partir",
+        "back": "to divide, to leave"
+      },
+      {
+        "front": "morir",
+        "back": "to die"
+      },
+      {
+        "front": "aceptar",
+        "back": "to accept, to approve, to agree to"
+      },
+      {
+        "front": "realizar",
+        "back": "to achieve, to attain, to accomplish"
+      },
+      {
+        "front": "suponer",
+        "back": "to suppose"
+      },
+      {
+        "front": "comprender",
+        "back": "to understand, to comprehend"
+      },
+      {
+        "front": "lograr",
+        "back": "to , to obtain, to achieve, to attain"
+      },
+      {
+        "front": "explicar",
+        "back": "to explain"
+      },
+      {
+        "front": "preguntar",
+        "back": "to ask, to inquire"
+      },
+      {
+        "front": "tocar",
+        "back": "to touch, to play (an instrument)"
+      },
+      {
+        "front": "reconocer",
+        "back": "to recognize"
+      },
+      {
+        "front": "estudiar",
+        "back": "to study"
+      },
+      {
+        "front": "alcanzar",
+        "back": "to reach, to catch up"
+      },
+      {
+        "front": "nacer",
+        "back": "to be born"
+      },
+      {
+        "front": "dirigir",
+        "back": "to direct"
+      },
+      {
+        "front": "correr",
+        "back": "to run"
+      },
+      {
+        "front": "utilizar",
+        "back": "to use, to utilize"
+      },
+      {
+        "front": "pagar",
+        "back": "to pay, to pay for"
+      },
+      {
+        "front": "ayudar",
+        "back": "to help"
+      },
+      {
+        "front": "gustar",
+        "back": "to please, to be pleasing"
+      },
+      {
+        "front": "jugar",
+        "back": "to play (a game or sort)"
+      },
+      {
+        "front": "escuchar",
+        "back": "to listen, to hear"
+      },
+      {
+        "front": "cumplir",
+        "back": "to fulfil, to carry out"
+      },
+      {
+        "front": "ofrecer",
+        "back": "to offer"
+      },
+      {
+        "front": "descubrir",
+        "back": "to discover"
+      },
+      {
+        "front": "levantar",
+        "back": "to raise, to lift"
+      },
+      {
+        "front": "intentar",
+        "back": "to try, to attempt"
+      }
+]
